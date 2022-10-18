a760e75f2b4f48f4f576b58f74f312805d71875e
felix-hauger <felix.hauger@laplateforme.io>
2022-10-18 10:09:55 +0200

    replaced "y" by " " for triangle spaces

--- Job09/triangle.py
+++ Job09/triangle.py
@@ -2,7 +2,7 @@ nb_hauteur = int(input("Entrez la hauteur de votre triangle >"))
 
 base = ""
 left_side = "/"
-right_side = "\\"
+right_side = ""
 
 for x in range(1, nb_hauteur):
     base += "__"
@@ -11,7 +11,7 @@ left_side = " " * nb_hauteur + left_side
 
 for x in range(0, nb_hauteur-1):
     left_side = left_side[1:]
-    right_side = "y" * x * 2 + "\\"
+    right_side = " " * x * 2 + "\\"
     print(left_side + right_side)
 
 print("/" + base + "\\")
