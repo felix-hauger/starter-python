2449abd1aae15d0093cdce1bc6c026709e5d71f6
felix-hauger <felix.hauger@laplateforme.io>
2022-10-18 09:36:23 +0200

    added triangle job

---
+++ Job09/triangle.py
@@ -0,0 +1,28 @@
+nb_hauteur = int(input("Entrez la hauteur de votre triangle >"))
+
+
+base = ""
+left_side = "/"
+right_side = "\\"
+
+
+for x in range(1, nb_hauteur):
+    base += "__"
+
+
+left_side = " " * nb_hauteur + left_side
+
+for x in range(0, nb_hauteur-1):
+    left_side = left_side[1:]
+    right_side = "y" * x * 2 + "\\"
+    print(left_side + right_side)
+
+
+# for x in range(1, nb_hauteur):
+#     left_side = left_side[1:]
+#     right_side = "y" * x + right_side
+#     right_side = right_side[:-1]
+#     print(left_side + right_side)
+
+
+print("/" + base + "\\")
