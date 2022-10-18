70a6707b0544d39313e2b99a29b0fc6c2d3f3a65
felix-hauger <felix.hauger@laplateforme.io>
2022-10-18 14:58:32 +0200

    removed useless argument in range loop in job09 - triangle

--- Job09/triangle.py
+++ Job09/triangle.py
@@ -4,7 +4,7 @@ base = "__" * int(nb_hauteur-1)
 left_side = " " * nb_hauteur + "/"
 right_side = ""
 
-for x in range(0, nb_hauteur-1):
+for x in range(nb_hauteur-1):
     left_side = left_side[1:]
     right_side = " " * x * 2 + "\\"
     print(left_side + right_side)
