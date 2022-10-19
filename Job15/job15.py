b2bffb0118649549a3490b717e3ca45525f01daa
felix-hauger <felix.hauger@laplateforme.io>
2022-10-19 13:45:54 +0200

    added a test in Job15

--- Job15/job15.py
+++ Job15/job15.py
@@ -1,5 +1,13 @@
 def sayHello(firstname, lastname):
-    print("Bonjour {} {}".format(firstname, lastname))
+    # test if first name is empty
+    if len(firstname) == 0:
+        print("Entrez un prénom")
+    # test if last name is empty, more test are possibles, for exemple the number and type of characters
+    elif len(lastname) == 0:
+        print("Entrez un nom de famille")
+    else:
+        # the brackets act as a placeholder for parameters
+        print("Bonjour {} {}".format(firstname, lastname))
 
 userinput_firstname = input("Quel est votre prénom ? > ")
 userinput_lastname = input("Quel est votre nom de famille ? > ")
