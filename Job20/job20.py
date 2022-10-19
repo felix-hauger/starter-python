514c9aa3466e209f1e15bfc13365cf635eb730ec
felix-hauger <felix.hauger@laplateforme.io>
2022-10-19 20:27:27 +0200

    added job20

---
+++ Job20/job20.py
@@ -0,0 +1,12 @@
+# Créer un programme job20.py. Le programme devra contenir une seule fonction
+# nommée myAddition qui prend en paramètres deux nombres et qui retourne le résultat
+# de leur addition.
+# Vous devrez appeler cette fonction et assigner à une variable le résultat qu’elle retourne.
+
+
+def myAddition(num1, num2):
+    return num1 + num2
+
+result = myAddition(89, 7)
+
+print(result)
